c47e7a96d0c9d135347290ad1ec8244170ac3855
Alexander Opalic <opalic@gmx.net>
2021-03-30 17:50:14 +0200

    added tailwind

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,8 +3,33 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const tailwindcss = require("tailwindcss");
+
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          tailwindcss
+        ],
+      },
+    },
+  },
+
+  plugins: [
+    {
+      use: "gridsome-plugin-tailwindcss",
+
+      // these options are optional, as they are copies of the default values...
+      options: {
+        tailwindConfig: './tailwind.config.js',
+        presetEnvConfig: {},
+        shouldImport: false,
+        shouldTimeTravel: false
+      }
+    },
+    // ... the rest of your plugins
+    ],
 }
