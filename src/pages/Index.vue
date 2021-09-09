0e483808c77205c8f56372712873ae1c52a3221e
Alexander Opalic <opalic@gmx.net>
2021-09-09 18:51:45 +0200

    updated cv

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -30,7 +30,7 @@
               Get in touch
             </button>
             <button
-              onClick='window.open("https://drive.google.com/file/d/1Wmtpwf_taeS3yOMREMXQT7nmblXKNlgl/view?usp=sharing");'
+              onClick='window.open("https://drive.google.com/file/d/1ESEODvvUqg81tXuLw9YAHIdQvkwP4dys/view?usp=sharing");'
               class="ml-4 inline-flex text-background bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-hover hover:text-primary rounded text-lg"
             >
               My resume
